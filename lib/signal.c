5984a672d169bde041f2c32de23e94d49f7515f8
Benjamin Moody <benjaminmoody@gmail.com>
2020-05-08 18:03:33 -0400

    Implement FLAC signal reading.
    When WFDB_FLAC_SUPPORT is defined, implement the functions
    flac_getsamp(), flac_isopen(), flac_isclose(), and flac_isseek().
    
    Decoding signals with libFLAC uses a push model; each time we call
    FLAC__stream_decoder_process_single(), the library decodes some
    arbitrarily defined block of samples, and invokes a callback function
    (iflac_samples()).  Since the buffers passed to the callback function
    may not be valid after the function returns, we need to copy the
    contents into a persistent buffer (ig->buf) until the application is
    ready to read them.
    
    While we're at it, it's useful for iflac_samples() to rearrange the
    samples into WFDB frame order, so that flac_getsamp() simply needs to
    read a value and advance a pointer.  For simplicity, the buffer is
    stored as an array of FLAC__int32, regardless of the input resolution.
    
    iflac_samples() also needs to accommodate a few eccentricities of the
    FLAC library and format:
    
    - We need to reject streams that contain the wrong number of signals
      or the wrong resolution.
    
    - If the input stream is invalid, libFLAC will sometimes try to fill
      in the gap with zeroes; we want to report the error to the
      application, rather than returning incorrect data.
    
    - To work around issues with FLAC__stream_decoder_seek_absolute(), we
      want to discard the first sample of each signal, after a seek.
    
    In addition to iflac_samples(), we need to define the functions
    iflac_read(), iflac_seek(), iflac_tell(), iflac_length(), and
    iflac_eof(), to allow a WFDB_FILE to be used as a source of input.
    Finally, we need to define a function iflac_error() that will be
    called in case of an invalid input stream.

--- lib/signal.c
+++ lib/signal.c
@@ -1369,7 +1369,399 @@ static void hsdfree(void)
     maxhsig = 0;
 }
 
-/* Routines for reading FLAC signal files. */
+/* Routines for reading FLAC signal files.
+
+   Terminology note: the FLAC format refers to an encoded block of
+   samples as a "frame".  In WFDB terminology, a "frame" is a group of
+   samples representing a fixed time interval (specified by the frame
+   frequency in the header file.)  A FLAC frame may represent the
+   contents of multiple WFDB frames, or only part of one.  Unless
+   otherwise noted, the word "frame" refers to a WFDB frame. */
+
+#ifdef WFDB_FLAC_SUPPORT
+
+/* The following functions (iflac_read, iflac_seek, iflac_tell,
+   iflac_length, iflac_eof, iflac_error, and iflac_samples) are used
+   as callbacks for the FLAC library.  The client_data argument is the
+   value passed to FLAC__stream_decoder_init_stream, which is a
+   pointer to a struct igdata. */
+
+/* iflac_read is called by the FLAC library in order to read a chunk of
+   data from the input file. */
+static FLAC__StreamDecoderReadStatus
+iflac_read(const FLAC__StreamDecoder *dec, FLAC__byte buffer[],
+	   size_t *bytes, void *client_data)
+{
+    struct igdata *g = client_data;
+    if (*bytes == 0)
+	return (FLAC__STREAM_DECODER_READ_STATUS_ABORT);
+    *bytes = wfdb_fread(buffer, sizeof(FLAC__byte), *bytes, g->fp);
+    if (wfdb_ferror(g->fp))
+	return (FLAC__STREAM_DECODER_READ_STATUS_ABORT);
+    else if (*bytes == 0)
+	return (FLAC__STREAM_DECODER_READ_STATUS_END_OF_STREAM);
+    else
+	return (FLAC__STREAM_DECODER_READ_STATUS_CONTINUE);
+}
+
+/* iflac_seek is called by the FLAC library in order to move to a
+   different position in the input file. */
+static FLAC__StreamDecoderSeekStatus
+iflac_seek(const FLAC__StreamDecoder *dec, FLAC__uint64 pos,
+	   void *client_data)
+{
+    struct igdata *g = client_data;
+    if (!g->seek)
+	return (FLAC__STREAM_DECODER_SEEK_STATUS_UNSUPPORTED);
+    else if (pos > LONG_MAX || wfdb_fseek(g->fp, pos, SEEK_SET))
+	return (FLAC__STREAM_DECODER_SEEK_STATUS_ERROR);
+    else
+	return (FLAC__STREAM_DECODER_SEEK_STATUS_OK);
+}
+
+/* iflac_tell is called by the FLAC library in order to determine the
+   current position in the input file. */
+static FLAC__StreamDecoderTellStatus
+iflac_tell(const FLAC__StreamDecoder *dec, FLAC__uint64 *pos,
+	   void *client_data)
+{
+    struct igdata *g = client_data;
+    long p;
+    if (!g->seek)
+	return (FLAC__STREAM_DECODER_TELL_STATUS_UNSUPPORTED);
+    else if ((p = wfdb_ftell(g->fp)) < 0)
+	return (FLAC__STREAM_DECODER_TELL_STATUS_ERROR);
+    else {
+	*pos = p;
+	return (FLAC__STREAM_DECODER_TELL_STATUS_OK);
+    }
+}
+
+/* iflac_length is called by the FLAC library in order to determine the
+   total size of the input file. */
+static FLAC__StreamDecoderLengthStatus
+iflac_length(const FLAC__StreamDecoder *dec, FLAC__uint64 *len,
+	     void *client_data)
+{
+    struct igdata *g = client_data;
+    long pos, end;
+
+    if (!g->seek)
+	return (FLAC__STREAM_DECODER_LENGTH_STATUS_UNSUPPORTED);
+    else if ((pos = wfdb_ftell(g->fp)) < 0)
+	return (FLAC__STREAM_DECODER_LENGTH_STATUS_ERROR);
+    else if (wfdb_fseek(g->fp, 0L, SEEK_END) ||
+	     (end = wfdb_ftell(g->fp)) < 0) {
+	wfdb_fseek(g->fp, pos, SEEK_SET);
+	return (FLAC__STREAM_DECODER_LENGTH_STATUS_ERROR);
+    }
+    if (wfdb_fseek(g->fp, pos, SEEK_SET))
+	return (FLAC__STREAM_DECODER_LENGTH_STATUS_ERROR);
+    *len = end;
+    return (FLAC__STREAM_DECODER_LENGTH_STATUS_OK);
+}
+
+/* iflac_eof is called by the FLAC library in order to check whether
+   we have reached the end of the input file. */
+static FLAC__bool iflac_eof(const FLAC__StreamDecoder *dec, void *client_data)
+{
+    struct igdata *g = client_data;
+    return (wfdb_feof(g->fp));
+}
+
+/* iflac_error is called by the FLAC library when the input stream
+   appears invalid or corrupted. */
+static void iflac_error(const FLAC__StreamDecoder *decoder,
+			FLAC__StreamDecoderErrorStatus status,
+			void *client_data)
+{
+    struct igdata *g = client_data;
+
+    switch (status) {
+      case FLAC__STREAM_DECODER_ERROR_STATUS_LOST_SYNC:
+	wfdb_error("getvec: unable to decode FLAC (lost sync)\n");
+	break;
+      case FLAC__STREAM_DECODER_ERROR_STATUS_BAD_HEADER:
+	wfdb_error("getvec: unable to decode FLAC (invalid header)\n");
+	break;
+      case FLAC__STREAM_DECODER_ERROR_STATUS_FRAME_CRC_MISMATCH:
+	wfdb_error("getvec: unable to decode FLAC (CRC mismatch)\n");
+	break;
+      case FLAC__STREAM_DECODER_ERROR_STATUS_UNPARSEABLE_STREAM:
+	wfdb_error("getvec: unable to decode FLAC (unsupported format)\n");
+	break;
+      default:
+	wfdb_error("getvec: unable to decode FLAC\n");
+	break;
+    }
+    /* Note that if an error is detected, the FLAC library will still
+       subsequently invoke iflac_samples, with a buffer of zeroes
+       rather than valid data. */
+    g->stat = -2;
+}
+
+/* iflac_samples is called by the FLAC library when a block of samples
+   has been decoded. */
+static FLAC__StreamDecoderWriteStatus
+iflac_samples(const FLAC__StreamDecoder *dec, const FLAC__Frame *ffrm,
+	      const FLAC__int32 *const buf[], void *client_data)
+{
+    struct igdata *g = client_data;
+    size_t nsig = ffrm->header.channels;
+    size_t nsamp = ffrm->header.blocksize;
+    size_t oldsize, newsize, frmsize, bufsize, spf, ipos, orem, s, n;
+    char *nbuf;
+    FLAC__int32 *p;
+
+    /* g->data is the number of signals in the group. */
+    if (nsig != g->data) {
+	wfdb_error("getvec: wrong number of signals in FLAC signal file\n");
+	g->stat = -2;
+    }
+    /* g->datb is the group sample resolution. */
+    if (ffrm->header.bits_per_sample > g->datb) {
+	wfdb_error("getvec: wrong sample resolution in FLAC signal file\n");
+	g->stat = -2;
+    }
+    /* If the resolution or number of signals is incorrect, or if an
+       error was previously detected by flac_error, then stop decoding
+       immediately (and return an error from getskewedframe or
+       isgsetframe.) */
+    if (g->stat < 0)
+	return (FLAC__STREAM_DECODER_WRITE_STATUS_ABORT);
+
+    /* g->buf is the start of the input buffer.
+
+       g->bp points to the next sample (FLAC__int32) to be retrieved.
+       This advances by four bytes with each call to flac_getsamp.
+       (When iflac_samples is called, g->bp should be located at the
+       start of a frame.)
+
+       g->packptr points to the next frame to be decoded.  This
+       advances by 4*nsig*spf bytes when a complete frame has been
+       decoded.
+
+       g->packcount is the number of samples (of each signal) that
+       have been decoded and stored at g->packptr.  This is initially
+       zero; when it reaches spf, it is reset to zero and g->packptr
+       advances.
+
+       g->be is the end of the input buffer.  We should always have
+       g->buf <= g->bp <= g->packptr <= g->be. */
+
+    spf = g->packspf;
+    newsize = nsig * nsamp * sizeof(FLAC__int32);
+    frmsize = nsig * spf * sizeof(FLAC__int32);
+
+    bufsize = g->be - g->buf;
+    oldsize = g->packptr - g->bp;
+
+    /* If the existing buffer is not large enough to hold old + new
+       data, plus one extra frame, it must be reallocated.  Typically
+       the FLAC block size is constant, so this reallocation will only
+       be needed once per file. */
+    if (bufsize < oldsize + newsize + frmsize) {
+	bufsize = oldsize + newsize + frmsize;
+	SUALLOC(nbuf, bufsize, 1);
+	if (!nbuf)
+	    return (FLAC__STREAM_DECODER_WRITE_STATUS_ABORT);
+
+	n = oldsize + (g->packcount ? frmsize : 0);
+	if (n)
+	    memcpy(nbuf, g->bp, n);
+	SFREE(g->buf);
+	g->buf = g->bp = nbuf;
+	g->be = nbuf + bufsize;
+	g->packptr = nbuf + oldsize;
+    }
+    /* Otherwise, shift existing data (if any) to the beginning of the
+       buffer. */
+    else if (g->bp != g->buf) {
+	n = oldsize + (g->packcount ? frmsize : 0);
+	if (n)
+	    memmove(g->buf, g->bp, n);
+	g->bp = g->buf;
+	g->packptr = g->buf + oldsize;
+    }
+
+    /* g->count is the number of initial samples to skip, following a
+       seek (see flac_isseek). */
+    if (g->count > nsamp) {
+	g->count -= nsamp;
+	return (FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE);
+    }
+    else {
+	ipos = g->count;
+	nsamp -= ipos;
+	g->count = 0;
+    }
+
+    /* Rearrange data from the buffer(s) provided by the decoder, and
+       store the samples in frame order. */
+    p = ((FLAC__int32 *) g->packptr) + g->packcount;
+    orem = spf - g->packcount;
+    while (nsamp != 0) {
+	n = (nsamp < orem ? nsamp : orem);
+	for (s = 0; s < nsig; s++) {
+	    memcpy(p, buf[s] + ipos, n * sizeof(FLAC__int32));
+	    p += spf;
+	}
+	nsamp -= n;
+	orem -= n;
+	if (orem == 0) {
+	    g->packptr += frmsize;
+	    p = (FLAC__int32 *) g->packptr;
+	    orem = spf;
+	    ipos += n;
+	}
+    }
+    g->packcount = spf - orem;
+
+    return (FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE);
+}
+
+/* Read and return the next sample from a FLAC signal file. */
+static int flac_getsamp(struct igdata *g)
+{
+    FLAC__int32 *ibp;
+    FLAC__StreamDecoderState state;
+    unsigned oldcount;
+
+    /* If the next frame has not yet been decoded, read more data from
+       the input file. */
+    while (g->bp == g->packptr) {
+	oldcount = g->packcount;
+	if (!FLAC__stream_decoder_process_single(g->flacdec)) {
+	    if (g->stat != -2) {
+		wfdb_error("getvec: unexpected FLAC decoding error\n");
+		g->stat = -2;
+	    }
+	    return (0);
+	}
+	if (g->stat <= 0) {
+	    return (0);
+	}
+	if (g->bp == g->packptr && g->packcount == oldcount) {
+	    state = FLAC__stream_decoder_get_state(g->flacdec);
+	    switch (state) {
+	      case FLAC__STREAM_DECODER_END_OF_STREAM:
+		if (g->packcount != 0)
+		    wfdb_error("getvec: warning: %d samples left over"
+			       " at end of file\n", g->packcount);
+		g->packcount = 0;
+		g->stat = 0;
+		return (0);
+	      case FLAC__STREAM_DECODER_SEARCH_FOR_METADATA:
+	      case FLAC__STREAM_DECODER_READ_METADATA:
+	      case FLAC__STREAM_DECODER_SEARCH_FOR_FRAME_SYNC:
+		break;
+	      default:
+		wfdb_error("getvec: unknown FLAC decoding state\n");
+		g->stat = -2;
+		return (0);
+	    }
+	}
+    }
+
+    /* Retrieve the next decoded sample. */
+    ibp = (FLAC__int32 *) g->bp;
+    g->bp = (char *) &ibp[1];
+    return (*ibp);
+}
+
+/* Open a FLAC stream decoder for an input signal group.  The input
+   file (ig->fp) has already been opened and the buffer (ig->buf) has
+   already been allocated. */
+static int flac_isopen(struct igdata *ig, struct hsdata **hs, unsigned ns)
+{
+    unsigned int i;
+
+    if (ns > FLAC__MAX_CHANNELS) {
+	wfdb_error(
+	    "isigopen: cannot store %u signals in a single FLAC file\n",
+	    ns);
+	return (-1);
+    }
+    for (i = 1; i < ns; i++) {
+	if (hs[i]->info.spf != hs[0]->info.spf) {
+	    wfdb_error(
+	       "isigopen: every signal in a FLAC file must have\n"
+	       "  the same sampling frequency\n");
+	    return (-1);
+	}
+    }
+
+    ig->flacdec = FLAC__stream_decoder_new();
+    if (!ig->flacdec) {
+	wfdb_error("isigopen: cannot initialize stream decoder\n");
+	return (-1);
+    }
+
+    ig->data = ns;
+    ig->datb = hs[0]->info.fmt - 500;
+    ig->count = 0;
+    ig->packspf = hs[0]->info.spf;
+    ig->packptr = ig->be = ig->bp = ig->buf + ig->bsize;
+    ig->packcount = 0;
+    if (FLAC__stream_decoder_init_stream(ig->flacdec, &iflac_read,
+					 &iflac_seek, &iflac_tell,
+					 &iflac_length, &iflac_eof,
+					 &iflac_samples, NULL,
+					 &iflac_error, ig)) {
+	wfdb_error("isigopen: cannot open stream decoder\n");
+	FLAC__stream_decoder_delete(ig->flacdec);
+	return (-1);
+    }
+    return (0);
+}
+
+/* Close a stream decoder. */
+static int flac_isclose(struct igdata *ig)
+{
+    FLAC__stream_decoder_delete(ig->flacdec);
+    return (0);
+}
+
+/* Seek to the given frame number in an input stream. */
+static int flac_isseek(struct igdata *ig, WFDB_Time t)
+{
+    FLAC__StreamDecoderState state;
+    WFDB_Time tt;
+
+    /* If there was a previous seek error, clear the decoder state so
+       we can try again.  (Calling flush at other times can break
+       decoding for some reason.) */
+    state = FLAC__stream_decoder_get_state(ig->flacdec);
+    if (state == FLAC__STREAM_DECODER_SEEK_ERROR)
+	FLAC__stream_decoder_flush(ig->flacdec);
+
+    /* Reset the block pointers to indicate nothing has been read
+       in the current block. */
+    ig->packptr = ig->bp = ig->be;
+    ig->packcount = 0;
+    ig->stat = 1;
+    ig->count = 0;
+
+    /* Seek to the desired sample.  Note that seek_absolute will
+       return an error if the given sample number is greater than or
+       equal to the length of the stream; isgsetframe should succeed
+       if tt is exactly the length of the stream.  Therefore, we
+       subtract one from the target sample number, and set ig->count
+       to 1 so that iflac_samples will discard the first sample from
+       each signal. */
+    tt = t * ig->packspf + ig->start;
+    if (tt != 0) {
+	tt--;
+	ig->count = 1;
+    }
+    /* This will invoke iflac_samples, with a (partial) block of
+       samples starting at the requested sample number. */
+    if (!FLAC__stream_decoder_seek_absolute(ig->flacdec, tt))
+	ig->stat = -1;
+    return (ig->stat);
+}
+
+#else /* !WFDB_FLAC_SUPPORT */
 
 static int flac_getsamp(struct igdata *ig)
 {
@@ -1392,6 +1784,8 @@ static int flac_isseek(struct igdata *ig, WFDB_Time t)
 {
     return (-1);
 }
+
+#endif
 
 /* Routines for writing FLAC signal files. */
 
