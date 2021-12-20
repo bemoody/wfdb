4c7a53de90486b883ce7a2ea3f672c35280e993c
Benjamin Moody <benjaminmoody@gmail.com>
2021-12-20 09:37:06 -0500

    install.sh: fail if files cannot be installed.

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # file: install.sh		G. Moody	7 June 2000
+#                 		Last revised:	20 December 2021
+
+set -e
 
 case $# in
  0|1) echo "usage: $0 DIRECTORY FILE [FILE ...]"
-      exit ;;
+      exit 1 ;;
 esac
 
 DIR=$1
@@ -15,7 +18,7 @@ fi
 if [ ! -d $DIR ]
 then
     echo "Can't install in $DIR -- exiting."
-    exit
+    exit 1
 fi
 
 shift
@@ -31,6 +34,7 @@ do
 	    cp -p $FILE $DIR
         else
 	    echo "$FILE does not exist"
+	    exit 1
         fi
     fi
 done
